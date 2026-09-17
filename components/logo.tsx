ee78226336ebb26d620fcca87a53e9c577255c48
Khatansaikhan <khatansaikhan.e@tavanbogd.com>
2026-09-17 17:32:25 +0800

    Replace site logo with Noyon House image

--- components/logo.tsx
+++ components/logo.tsx
@@ -1,10 +1,10 @@
 import Link from "next/link";
+import Image from "next/image";
 
 export function Logo({ inverse = false }: { inverse?: boolean }) {
   return (
     <Link href="/" className={`logo ${inverse ? "logo-inverse" : ""}`} aria-label="NOYON HOUSE нүүр">
-      <span className="logo-mark" aria-hidden="true"><i /><i /><i /></span>
-      <span><b>NOYON HOUSE</b><small>КАНАД ТЕХНОЛОГИ · МОНГОЛ АХУЙ</small></span>
+      <Image className="logo-image" src="/images/NoyonHouse.jpg" alt="NOYON House construction" width={190} height={64} priority style={{ objectFit: "cover" }} />
     </Link>
   );
 }
