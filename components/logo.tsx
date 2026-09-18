6edb27ba91d648c379f42a0c17332c81549ee938
Khatansaikhan <khatansaikhan.e@tavanbogd.com>
2026-09-18 16:18:06 +0800

    Use correct logo on admin login

--- components/logo.tsx
+++ components/logo.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 export function Logo({ inverse = false }: { inverse?: boolean }) {
   return (
     <Link href="/" className={`logo ${inverse ? "logo-inverse" : ""}`} aria-label="Ноён хаус нүүр хуудас">
-      <Image className="logo-image" src="/images/LOGO.png" alt="Ноён хаус construction" width={200} height={100} priority />
+      <Image className="logo-image" src={inverse ? "/images/LOGO.png" : "/images/NoyonHouse2.png"} alt="Ноён хаус construction" width={200} height={100} priority />
     </Link>
   );
 }
